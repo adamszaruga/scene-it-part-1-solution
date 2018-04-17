a65205f17885f820f62d8077d1f5ef380daf83c1
Adam Szaruga <adamszaruga@gmail.com>
2018-04-17 19:44:02 -0400

    implemented part 1

---
+++ index.js
@@ -0,0 +1,37 @@
+$(function(){
+
+	function renderMovies(moviesArray) {
+
+		var finalHTML = "";
+
+		moviesArray.forEach(function(currentMovie){
+
+			finalHTML += '<div class="card" style="width: 20rem;">';
+			finalHTML += '<img class="card-img-top" src="'+ currentMovie.Poster +'"/>';
+			finalHTML += '<div class="card-body">';
+			finalHTML += '<h4 class="card-title">'+ currentMovie.Title;
+			finalHTML += '<span class="badge badge-secondary float-right">'+ currentMovie.Year +'</span>';
+			finalHTML += '</h4>';
+			finalHTML += '<a href="#" class="btn btn-primary addButton">Add!</a>';
+			finalHTML += '</div>';
+			finalHTML += '</div>';
+		});
+
+		return finalHTML;
+	}
+
+	$('form').submit(function(e){
+		e.preventDefault();
+		var movieHTML = renderMovies(movieData);
+		$('.movie-container').html(movieHTML);
+	});
+
+
+
+})
+
+
+
+
+
+
